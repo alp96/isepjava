bfdc85423fba8ff4ed6ed85875a35c05015062f3
rdcdt1 <wanduchalard@gmail.com>
2018-05-25 15:48:23 +0200

    changement

--- Main.java
+++ Main.java
@@ -3,4 +3,17 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
     }
+
+    public debutPartie(){//changer cavector<Continent> listecontinent){
+	courant_->jouer();
+	//idee mais a modifier
+	for(int j = 0; j < listecontinent.size(); ++j){
+		for(int i = 0; i < listecontinent.at(j).getNbterritoire(); ++i){
+			fenetre_.draw(listecontinent.at(j).getlisteterritoire(i).getSprite());
+		}
+	}
+	return true;
+}
+
+
 }
--- Region.java
+++ Region.java
@@ -17,39 +17,14 @@ public class Region {
      */
     public String name;
 
-    /**
-     * 
-     */
-    /**public void boolean{
 
-    }*/
-
-
-    /**
-     * @param name
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return
-     */
+ 
     public String getName() {
         return this.name;
     }
 
-    /**
-     * @param listTerritoire
-     */
-    public void setTheBiggest(int listTerritoire) {
-    }
-
-    /**
-     * @return
-     * TODO Set the biggest (maybe not in this class)
-     */
-    public Region getTheBiggest() {
-        return null;
-    }
 }
